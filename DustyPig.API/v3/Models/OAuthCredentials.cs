92eb766e7ec136b4b9c512bb13084eb88ad9b84b
JD <dev@dustypig.tv>
2022-04-08 04:41:20 -0400

    Add OAuth login

---
+++ DustyPig.API/v3/Models/OAuthCredentials.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace DustyPig.API.v3.Models
+{
+    public enum OAuthCredentialProviders
+    {
+        Apple,
+        Facebook,
+        Google,
+        Microsoft,
+        Twitter
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class OAuthCredentials
+    {
+
+        [JsonRequired]
+        [JsonProperty("provider")]
+        public OAuthCredentialProviders Provider { get; set; }
+
+        [JsonRequired]
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
