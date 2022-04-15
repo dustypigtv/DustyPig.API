49af133e771b7f1d4531d27d71c5486caa373cad
JD <dev@dustypig.tv>
2022-04-15 04:08:09 -0600

    Add token to CreateAccountResponse, to less # of calls to server

--- DustyPig.API/v3/Models/CreateAccountResponse.cs
+++ DustyPig.API/v3/Models/CreateAccountResponse.cs
@@ -6,5 +6,11 @@ namespace DustyPig.API.v3.Models
     {
         [JsonProperty("email_verification_required")]
         public bool EmailVerificationRequired { get; set; }
+
+        /// <summary>
+        /// This will be null if <see cref="EmailVerificationRequired"/> == true
+        /// </summary>
+        [JsonProperty("token")]
+        public string Token { get; set; }
     }
 }
