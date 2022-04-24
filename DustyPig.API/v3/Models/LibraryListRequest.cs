0903e7dfd242713c7a939e0dc6153cc1fcc4c6fa
JD <dev@dustypig.tv>
2022-04-24 11:23:35 -0600

    Fix Validate method

--- DustyPig.API/v3/Models/LibraryListRequest.cs
+++ DustyPig.API/v3/Models/LibraryListRequest.cs
@@ -9,11 +9,13 @@ namespace DustyPig.API.v3.Models
         [JsonProperty("library_id")]
         public int LibraryId { get; set; }
 
-        public void Validate()
+        public new void Validate()
         {
             var lst = new List<string>();
 
             Validators.ValidateId(nameof(LibraryId), LibraryId, lst);
+            try { base.Validate(); }
+            catch (ModelValidationException ex) { lst.AddRange(ex.Errors); }
 
             if (lst.Count > 0)
                 throw new ModelValidationException { Errors = lst };
