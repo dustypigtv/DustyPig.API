30395d792ee5291ecedab8bc0a1a9defd7aaca9d
JD <dev@dustypig.tv>
2022-05-10 09:30:15 -0600

    Better query/tokenization logic

--- DustyPig.API/v3/StringExtensions/StringUtils.cs
+++ DustyPig.API/v3/StringExtensions/StringUtils.cs
@@ -203,6 +203,10 @@ namespace System
             str = str.Replace("-", " ").Replace("/", " ");
             var tokens = Tokenize(str);
 
+            for (int i = 0; i < tokens.Count; i++)
+                if (long.TryParse(tokens[i], out long value))
+                    tokens[i] = NumberToWords(value);
+
             return string.Join(" ", tokens).Trim();
         }
 
@@ -223,6 +227,68 @@ namespace System
             var ret = new List<string>(str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
             
             return ret;
+        }
+
+        public static string NumberToWords(long number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+
+            if ((number / 1000000000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " trillion ";
+                number %= 1000000000000;
+            }
+
+            if ((number / 1000000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " billion ";
+                number %= 1000000000;
+            }
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
         }
 
         public static string FixSpaces(string text)
