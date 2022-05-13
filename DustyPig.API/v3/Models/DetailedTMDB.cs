519f51e7c2baf4f052d651856a53574b4042a582
JD <dev@dustypig.tv>
2022-05-13 08:24:57 -0600

    Add BackdropUrl

--- DustyPig.API/v3/Models/DetailedTMDB.cs
+++ DustyPig.API/v3/Models/DetailedTMDB.cs
@@ -7,6 +7,9 @@ namespace DustyPig.API.v3.Models
     [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class DetailedTMDB : BasicTMDB
     {
+        [JsonProperty("backdrop_url")]
+        public string BackdropUrl { get; set; }
+
         [JsonProperty("rated")]
         public string Rated { get; set; }
 
