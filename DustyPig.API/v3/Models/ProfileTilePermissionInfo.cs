06f75f41ed6db071211b33be4e6e702c177e0a9a
JD <dev@dustypig.tv>
2022-06-05 08:53:28 -0600

    Added HasLibraryAccess flag

--- DustyPig.API/v3/Models/ProfileTilePermissionInfo.cs
+++ DustyPig.API/v3/Models/ProfileTilePermissionInfo.cs
@@ -5,6 +5,9 @@ namespace DustyPig.API.v3.Models
     [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class ProfileTilePermissionInfo : BasicProfile
     {
+        [JsonProperty("has_library_access")]
+        public bool HasLibraryAccess { get; set; }
+
         [JsonProperty("can_watch_by_default")]
         public bool CanWatchByDefault { get; set; }
 
