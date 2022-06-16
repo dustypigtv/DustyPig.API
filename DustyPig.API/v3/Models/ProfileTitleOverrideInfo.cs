42578f832f9ed934e72f5e6c8d26aa8b69dc3964
JD <dev@dustypig.tv>
2022-06-16 09:33:20 -0600

    Update TitleOverrides for batch processing

---
+++ DustyPig.API/v3/Models/ProfileTitleOverrideInfo.cs
@@ -0,0 +1,28 @@
+﻿using DustyPig.API.v3.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DustyPig.API.v3.Models
+{
+    public class ProfileTitleOverrideInfo : IValidate
+    {
+        [JsonProperty("profile_id")]
+        public int ProfileId { get; set; }
+
+        [JsonProperty("new_state")]
+        public OverrideState NewState { get; set; }
+
+        public void Validate()
+        {
+            var lst = new List<string>();
+
+            Validators.ValidateId(nameof(ProfileId), ProfileId, lst);
+
+            if (!(NewState == OverrideState.Allow || NewState == OverrideState.Block || NewState == OverrideState.Default))
+                lst.Add($"Invalid {nameof(NewState)}");
+
+            if (lst.Count > 0)
+                throw new ModelValidationException { Errors = lst };
+        }
+    }
+}
--- DustyPig.API/v3/Models/TitleOverride.cs
+++ DustyPig.API/v3/Models/TitleOverride.cs
@@ -1,22 +1,30 @@
 ﻿using DustyPig.API.v3.Interfaces;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace DustyPig.API.v3.Models
 {
     public class TitleOverride : IValidate
     {
+        [JsonProperty("media_entry_id")]
         public int MediaEntryId { get; set; }
 
-        public int ProfileId { get; set; }
-
-        public OverrideState State { get; set; }
+        [JsonProperty("overrides")]
+        public List<ProfileTitleOverrideInfo> Overrides { get; set; } = new List<ProfileTitleOverrideInfo>();
 
         public void Validate()
         {
             var lst = new List<string>();
 
             Validators.ValidateId(nameof(MediaEntryId), MediaEntryId, lst);
-            Validators.ValidateId(nameof(ProfileId), ProfileId, lst);
+
+            if (Overrides == null || Overrides.Count == 0)
+                lst.Add($"{nameof(Overrides)} may not be empty");
+            else
+                foreach (var item in Overrides)
+                    try { item.Validate(); }
+                    catch (ModelValidationException ex) { lst.AddRange(ex.Errors); }
+
 
             if (lst.Count > 0)
                 throw new ModelValidationException { Errors = lst };
