65a96db5c42974dbbe7879d4aa8c8d24966888ce
JD <dev@dustypig.tv>
2022-06-24 14:06:34 -0600

    Make BasicFriend sortable

--- DustyPig.API/v3/Models/BasicFriend.cs
+++ DustyPig.API/v3/Models/BasicFriend.cs
@@ -1,14 +1,21 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace DustyPig.API.v3.Models
 {
     [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
-    public class BasicFriend
+    public class BasicFriend : IComparable
     {
         [JsonProperty("id")]
         public int Id { get; set; }
 
         [JsonProperty("display_name")]
         public string DisplayName { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var comp = obj as BasicFriend;
+            return DisplayName.CompareTo(comp.DisplayName);
+        }
     }
 }
