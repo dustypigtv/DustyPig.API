14d7431a9b7f292ff7670828ab36c18791e37b4e
JD <dev@dustypig.tv>
2023-04-19 23:01:52 -0600

    Change response type to LoginResponse

--- DustyPig.API/v3/Clients/AuthClient.cs
+++ DustyPig.API/v3/Clients/AuthClient.cs
@@ -190,8 +190,8 @@ namespace DustyPig.API.v3.Clients
         /// <summary>
         /// Verifies an auth token and returns the type
         /// </summary>
-        /// <param name="fcmToken">Include the FirebaseCloudMessaging token to also valid it as part of the call. If not null and validation fails, the auth token is invalided on the server and this call return 401 (Unauthorized), requiring a new login</param>
-        public Task<Response<VerifyTokenResponse>> VerifyAuthTokenAsync(string fcmToken = null, CancellationToken cancellationToken = default) =>
-            _client.PostAsync<VerifyTokenResponse>(true, PREFIX + "VerifyAuthToken", new SimpleValue<string>(fcmToken + string.Empty),  cancellationToken);
+        /// <param name="fcmToken">Include the FirebaseCloudMessaging token to also valid it as part of the call.</param>
+        public Task<Response<LoginResponse>> VerifyAuthTokenAsync(string fcmToken = null, CancellationToken cancellationToken = default) =>
+            _client.PostAsync<LoginResponse>(true, PREFIX + "VerifyAuthToken", new SimpleValue<string>(fcmToken + string.Empty),  cancellationToken);
     }
 }
