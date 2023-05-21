f4baba158a738c57dc2cec7725d3d3cf3d9fc86a
JD <dev@dustypig.tv>
2023-05-20 23:44:28 -0600

    Remove unused artwork fields

--- DustyPig.API/v3/Models/BasicPlaylist.cs
+++ DustyPig.API/v3/Models/BasicPlaylist.cs
@@ -10,15 +10,6 @@ namespace DustyPig.API.v3.Models
 
         [JsonProperty("artwork_url_1")]
         [JsonRequired]
-        public string ArtworkUrl1 { get; set; }
-
-        [JsonProperty("artwork_url_2")]
-        public string ArtworkUrl2 { get; set; }
-
-        [JsonProperty("artwork_url_3")]
-        public string ArtworkUrl3 { get; set; }
-
-        [JsonProperty("artwork_url_4")]
-        public string ArtworkUrl4 { get; set; }
+        public string ArtworkUrl { get; set; }
     }
 }
