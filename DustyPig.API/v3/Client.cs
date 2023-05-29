d9be2af7106ca040c8796ff88e91bc81da48004d
JD <dev@dustypig.tv>
2023-05-29 02:12:32 -0600

    Use ResponseWrapper from API to get better error info across different client platforms

--- DustyPig.API/v3/Client.cs
+++ DustyPig.API/v3/Client.cs
@@ -26,13 +26,6 @@ namespace DustyPig.API.v3
             set => _client.IncludeRawContentInResponse = value;
         }
 
-        public static bool AutoThrowIfError
-        {
-            get => _client.AutoThrowIfError;
-            set => _client.AutoThrowIfError = value;
-        }
-
-
         public Client()
         {
 #if DEBUG
@@ -40,10 +33,10 @@ namespace DustyPig.API.v3
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 #endif
         }
-        
+
 
         public static Version APIVersion => typeof(Client).Assembly.GetName().Version;
-        
+
         public static Uri BaseUrl
         {
             get => _client.BaseAddress;
@@ -78,7 +71,7 @@ namespace DustyPig.API.v3
 
 
 
-        #region API Calls
+
 
 
 
@@ -90,52 +83,43 @@ namespace DustyPig.API.v3
             return ret;
         }
 
-        internal Task<Response> GetAsync(bool tokenNeeded, string url, CancellationToken cancellationToken) =>
-            _client.GetAsync(url, null, GetHeaders(tokenNeeded), cancellationToken);
-        
 
-        internal Task<Response<T>> GetAsync<T>(bool tokenNeeded, string url, CancellationToken cancellationToken) => 
-            _client.GetAsync<T>(url, null, GetHeaders(tokenNeeded), cancellationToken);
-        
+        internal async Task<Response> GetAsync(bool tokenNeeded, string url, CancellationToken cancellationToken)
+        {
+            var response = await _client.GetAsync<ResponseWrapper>(url, null, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToResponse(response);
+        }
+
+        internal async Task<Response<T>> GetAsync<T>(bool tokenNeeded, string url, CancellationToken cancellationToken)
+        {
+            var response = await _client.GetAsync<ResponseWrapper<T>>(url, null, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToDataResponse(response);
+        }
 
         internal async Task<Response<T>> GetSimpleAsync<T>(bool tokenNeeded, string url, CancellationToken cancellationToken)
         {
-            var ret = await _client.GetAsync<SimpleValue<T>>(url, null, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
-            if (ret.Success)
-                return new Response<T>
-                {
-                    Data = ret.Data.Value,
-                    Success = true,
-                    RawContent = ret.RawContent,
-                    ReasonPhrase = ret.ReasonPhrase,
-                    StatusCode = ret.StatusCode
-                };
-
-            return new Response<T> 
-            {
-                Error = ret.Error,
-                RawContent = ret.RawContent,
-                ReasonPhrase = ret.ReasonPhrase,
-                StatusCode = ret.StatusCode
-            };
+            var response = await _client.GetAsync<ResponseWrapper<SimpleValue<T>>>(url, null, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToSimpleResponse(response);
         }
 
-        internal Task<Response> PostAsync(bool tokenNeeded, string url, object data, CancellationToken cancellationToken)
+        internal async Task<Response> PostAsync(bool tokenNeeded, string url, object data, CancellationToken cancellationToken)
         {
             if (data is IValidate iv)
                 try { iv.Validate(); }
-                catch (ModelValidationException ex) { return Task.FromResult(new Response { Error = ex }); }
-           
-            return _client.PostAsync(url, data, GetHeaders(tokenNeeded), cancellationToken);
+                catch (ModelValidationException ex) { return new Response { Error = ex }; }
+
+            var response = await _client.PostAsync<ResponseWrapper>(url, data, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToResponse(response);
         }
 
-        internal Task<Response<T>> PostAsync<T>(bool tokenNeeded, string url, object data, CancellationToken cancellationToken)
+        internal async Task<Response<T>> PostAsync<T>(bool tokenNeeded, string url, object data, CancellationToken cancellationToken)
         {
             if (data is IValidate iv)
                 try { iv.Validate(); }
-                catch (ModelValidationException ex) { return Task.FromResult(new Response<T> { Error = ex }); }
+                catch (ModelValidationException ex) { return new Response<T> { Error = ex }; }
 
-            return _client.PostAsync<T>(url, data, GetHeaders(tokenNeeded), cancellationToken);
+            var response = await _client.PostAsync<ResponseWrapper<T>>(url, data, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToDataResponse(response);
         }
 
         internal async Task<Response<T>> PostWithSimpleResponseAsync<T>(bool tokenNeeded, string url, object data, CancellationToken cancellationToken)
@@ -144,30 +128,85 @@ namespace DustyPig.API.v3
                 try { iv.Validate(); }
                 catch (ModelValidationException ex) { return new Response<T> { Error = ex }; }
 
-            var ret = await _client.PostAsync<SimpleValue<T>>(url, data, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
-            if (ret.Success)
-                return new Response<T>
-                {
-                    Data = ret.Data.Value,
-                    Success = true,
-                    RawContent = ret.RawContent,
-                    ReasonPhrase = ret.ReasonPhrase,
-                    StatusCode = ret.StatusCode
-                };
-
-            return new Response<T>
-            {
-                Error = ret.Error,
-                RawContent = ret.RawContent,
-                ReasonPhrase = ret.ReasonPhrase,
-                StatusCode = ret.StatusCode
-            };
+            var response = await _client.PostAsync<ResponseWrapper<SimpleValue<T>>>(url, data, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToSimpleResponse(response);
         }
 
-        internal Task<Response> DeleteAsync(bool tokenNeeded, string url, CancellationToken cancellationToken) =>
-            _client.DeleteAsync(url, null, GetHeaders(tokenNeeded), cancellationToken);
-        
-        #endregion
+        internal async Task<Response> DeleteAsync(bool tokenNeeded, string url, CancellationToken cancellationToken)
+        {
+            var response = await _client.DeleteAsync<ResponseWrapper>(url, null, GetHeaders(tokenNeeded), cancellationToken).ConfigureAwait(false);
+            return ConvertToResponse(response);
+        }
 
+
+
+
+        static Response ConvertToResponse(Response<ResponseWrapper> rw)
+        {
+            if (rw.Success)
+                return new Response
+                {
+                    Success = rw.Data.Success,
+                    Error = rw.Data.Success ? null : new Exception(rw.Data.Error),
+                    RawContent = rw.RawContent,
+                    ReasonPhrase = rw.ReasonPhrase,
+                    StatusCode = rw.StatusCode
+                };
+            else
+                return new Response
+                {
+                    Success = false,
+                    Error = rw.Error,
+                    RawContent = rw.RawContent,
+                    ReasonPhrase = rw.ReasonPhrase,
+                    StatusCode = rw.StatusCode
+                };
+        }
+
+        static Response<T> ConvertToDataResponse<T>(Response<ResponseWrapper<T>> rw)
+        {
+            if (rw.Success)
+                return new Response<T>
+                {
+                    Success = rw.Data.Success,
+                    Error = rw.Data.Success ? null : new Exception(rw.Data.Error),
+                    Data = rw.Data.Data,
+                    RawContent = rw.RawContent,
+                    ReasonPhrase = rw.ReasonPhrase,
+                    StatusCode = rw.StatusCode
+                };
+            else
+                return new Response<T>
+                {
+                    Success = false,
+                    Error = rw.Error,
+                    RawContent = rw.RawContent,
+                    ReasonPhrase = rw.ReasonPhrase,
+                    StatusCode = rw.StatusCode
+                };
+        }
+
+        static Response<T> ConvertToSimpleResponse<T>(Response<ResponseWrapper<SimpleValue<T>>> rw)
+        {
+            if (rw.Success)
+                return new Response<T>
+                {
+                    Success = rw.Data.Success,
+                    Error = rw.Data.Success ? null : new Exception(rw.Data.Error),
+                    Data = rw.Data.Data.Value,
+                    RawContent = rw.RawContent,
+                    ReasonPhrase = rw.ReasonPhrase,
+                    StatusCode = rw.StatusCode
+                };
+            else
+                return new Response<T>
+                {
+                    Success = false,
+                    Error = rw.Error,
+                    RawContent = rw.RawContent,
+                    ReasonPhrase = rw.ReasonPhrase,
+                    StatusCode = rw.StatusCode
+                };
+        }
     }
 }
---
+++ DustyPig.API/v3/Models/ResponseWrapper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace DustyPig.API.v3.Models
+{
+    public class ResponseWrapper
+    {
+        /// <summary>
+        /// Whether the operation was successful
+        /// </summary>
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// If <see cref="Success"/> == false, then this is the error message
+        /// </summary>
+        [JsonProperty("error")]
+        public string Error { get; set; }
+    }
+
+    public class ResponseWrapper<T>
+    {
+        /// <summary>
+        /// Whether the operation was successful
+        /// </summary>
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// If <see cref="Success"/> == true, then this is the json response
+        /// </summary>
+        [JsonProperty("data")]
+        public T Data { get; set; }
+
+        /// <summary>
+        /// If <see cref="Success"/> == false, then this is the error message
+        /// </summary>
+        [JsonProperty("error")]
+        public string Error { get; set; }
+    }
+}
