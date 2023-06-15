9bfd5da991214b6409eb8da1bca1912eb59c6671
JD <dev@dustypig.tv>
2023-06-15 02:37:45 -0600

    Add profile name and avatar url for ProfileTitleOverrideInfo

--- DustyPig.API/v3/Models/ProfileTitleOverrideInfo.cs
+++ DustyPig.API/v3/Models/ProfileTitleOverrideInfo.cs
@@ -9,6 +9,20 @@ namespace DustyPig.API.v3.Models
         [JsonProperty("profile_id")]
         public int ProfileId { get; set; }
 
+        /// <summary>
+        /// This is filled in for <see cref="Clients.MediaClient.GetTitlePermissionsAsync(int, System.Threading.CancellationToken)"/>, and is ignored for <see cref="Clients.MediaClient.SetTitlePermissionsAsync(TitlePermissionInfo, System.Threading.CancellationToken)"/>
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// This is filled in for <see cref="Clients.MediaClient.GetTitlePermissionsAsync(int, System.Threading.CancellationToken)"/>, and is ignored for <see cref="Clients.MediaClient.SetTitlePermissionsAsync(TitlePermissionInfo, System.Threading.CancellationToken)"/>
+        /// </summary>
+        [JsonProperty("avatar_url")]
+        public string AvatarUrl { get; set; }
+
+
         [JsonProperty("state")]
         public OverrideState State { get; set; }
 
@@ -20,6 +34,8 @@ namespace DustyPig.API.v3.Models
 
             if (!(State == OverrideState.Allow || State == OverrideState.Block || State == OverrideState.Default))
                 lst.Add($"Invalid {nameof(State)}");
+
+            //Don't validate Name and AvatarUrl. They set by the server for GetTitlePermissions, and ignored for SetTitlePermissions
 
             if (lst.Count > 0)
                 throw new ModelValidationException { Errors = lst };
