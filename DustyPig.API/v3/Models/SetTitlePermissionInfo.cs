a8b119e12ac011c870a05ccda67eeed135295076
JD <dev@dustypig.tv>
2023-10-08 00:01:04 -0600

    Added validation to SetTitlePermissionInfo

--- DustyPig.API/v3/Models/SetTitlePermissionInfo.cs
+++ DustyPig.API/v3/Models/SetTitlePermissionInfo.cs
@@ -1,10 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using DustyPig.API.v3.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DustyPig.API.v3.Models
 {
-    public class SetTitlePermissionInfo : IEquatable<SetTitlePermissionInfo>
+    public class SetTitlePermissionInfo : IValidate, IEquatable<SetTitlePermissionInfo>
     {
         [JsonProperty("media_id")]
         public int MediaId { get; set; }
@@ -14,6 +16,29 @@ namespace DustyPig.API.v3.Models
 
         [JsonProperty("override_state")]
         public OverrideState OverrideState { get; set; }
+
+        #region IValidate
+
+        public void Validate()
+        {
+            var lst = new List<string>();
+
+            Validators.ValidateId(nameof(MediaId), MediaId, lst);
+            Validators.ValidateId(nameof(ProfileId), ProfileId, lst);
+            if (new OverrideState[] { OverrideState.Allow, OverrideState.Block, OverrideState.Default }.Contains(OverrideState) == false)
+                lst.Add($"Invalid {nameof(OverrideState)}");
+
+            Validators.ValidateId(nameof(MediaId), MediaId, lst);
+
+            if (lst.Count > 0)
+                throw new ModelValidationException { Errors = lst };
+        }
+
+
+        #endregion
+
+
+        #region IEquatable
 
         public override bool Equals(object obj)
         {
@@ -46,5 +71,7 @@ namespace DustyPig.API.v3.Models
         {
             return !(left == right);
         }
+
+        #endregion
     }
 }
