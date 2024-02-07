5171d9c98e4744ab60847bb0e1f83bde0a27d0c3
Jason <dev@dustypig.tv>
2024-02-07 14:43:25 -0700

    Added implicit converters

--- DustyPig.API/v3/Models/Result.cs
+++ DustyPig.API/v3/Models/Result.cs
@@ -4,7 +4,7 @@ namespace DustyPig.API.v3.Models
 {
     public class Result
     {
-        public bool Success { get; set; }   
+        public bool Success { get; set; }
 
         public string Error { get; set; }
 
@@ -12,21 +12,81 @@ namespace DustyPig.API.v3.Models
 
         public static Result BuildError(string error) => new() { Error = error };
 
-        public static Result BuildError(Exception ex) => new() {  Error = ex.Message };
+        public static Result BuildError(Exception ex) => new() { Error = ex.Message };
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="ex"/> to an <see cref="Result"/> and sets <see cref="Error"/> = <paramref name="ex"/>.ToString().
+        /// </summary>
+        /// <param name="ex">The value to convert.</param>
+        public static implicit operator Result(ModelValidationException ex) => BuildError(ex.ToString());
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="ex"/> to an <see cref="Result"/> and sets <see cref="Error"/> = <paramref name="ex"/>.Message.
+        /// </summary>
+        /// <param name="ex">The value to convert.</param>
+        public static implicit operator Result(Exception ex) => BuildError(ex.Message);
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="error"/> to an <see cref="Result"/> and sets <see cref="Error"/> = <paramref name="error"/>.
+        /// </summary>
+        /// <param name="error">The value to convert.</param>
+        public static implicit operator Result(string error) => BuildError(error);
+
     }
 
+
+
+
+
     public class Result<T>
-    { 
+    {
         public bool Success { get; set; }
 
         public string Error { get; set; }
 
         public T Data { get; set; }
 
-        public static Result<T> BuildSuccess(T data) => new Result<T> { Success = true, Data = data };
+        public static Result<T> BuildSuccess(T data) => new() { Success = true, Data = data };
 
-        public static Result<T> BuildError(string error) => new Result<T> { Error = error };
+        public static Result<T> BuildError(string error) => new() { Error = error };
 
-        public static Result<T> BuildError(Exception ex) => new Result<T> { Error = ex.Message };
+        public static Result<T> BuildError(Exception ex) => new() { Error = ex.Message };
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="ex"/> to an <see cref="Result{T}"/> and sets <see cref="Error"/> = <paramref name="ex"/>.ToString().
+        /// </summary>
+        /// <param name="ex">The value to convert.</param>
+        public static implicit operator Result<T>(ModelValidationException ex) => BuildError(ex.ToString());
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="ex"/> to an <see cref="Result{T}"/> and sets <see cref="Error"/> = <paramref name="ex"/>.Message.
+        /// </summary>
+        /// <param name="ex">The value to convert.</param>
+        public static implicit operator Result<T>(Exception ex) => BuildError(ex.Message);
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="value"/> to an <see cref="Result{T}"/> and sets <see cref="Success"/> = true.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        public static implicit operator Result<T>(T value) => BuildSuccess(value);
+
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="error"/> to an <see cref="Result{T}"/> and sets <see cref="Error"/> = <paramref name="error"/>.
+        /// </summary>
+        /// <param name="error">The value to convert.</param>
+        public static implicit operator Result<T>(string error) => BuildError(error);
+
+        /// <summary>
+        /// Implicitly converts the specified <paramref name="result"/> to an <see cref="Result{T}"/>.
+        /// </summary>
+        /// <param name="result">The <see cref="Result"/>.</param>
+        public static implicit operator Result<T>(Result result) => new Result<T> { Success = result.Success, Error = result.Error };
     }
 }
