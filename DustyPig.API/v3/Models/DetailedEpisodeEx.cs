4c6df8b9716ec1e0bdc359cc6149ca030c16adda
Jason <dev@dustypig.tv>
2024-03-20 12:23:04 -0600

    Include DetailedSeries in DetailedEpisodeEx

--- DustyPig.API/v3/Models/DetailedEpisodeEx.cs
+++ DustyPig.API/v3/Models/DetailedEpisodeEx.cs
@@ -5,9 +5,5 @@
 /// </summary>
 public class DetailedEpisodeEx : DetailedEpisode
 {
-    public string SeriesTitle { get; set; }
-
-    public string SeriesArtworkUrl { get; set; }
-
-    public string SeriesBackdropUrl { get; set; }
+   public DetailedSeries Series { get; set; }
 }
