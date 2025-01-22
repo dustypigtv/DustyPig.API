52bf6ab2a5f2c83ae2192d8f098fda5bc0c0c6f9
Jason Davis <jasondavis303@gmail.com>
2025-01-21 19:16:58 -0700

    Added timestamp to playback progress

--- DustyPig.API/v3/Models/PlaybackProgress.cs
+++ DustyPig.API/v3/Models/PlaybackProgress.cs
@@ -1,4 +1,5 @@
 ﻿using DustyPig.API.v3.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace DustyPig.API.v3.Models;
@@ -15,6 +16,11 @@ public class PlaybackProgress : IValidate
     /// </summary>
     public double Seconds { get; set; }
 
+    /// <summary>
+    /// UTC Date-Timestamp of when this progress occured. It helps the server keep offline watch progress in sync
+    /// </summary>
+    public DateTime AsOfUTC { get; set; }
+
     public void Validate()
     {
         var lst = new List<string>();
@@ -22,6 +28,9 @@ public class PlaybackProgress : IValidate
         Validators.ValidateId(nameof(Id), Id, lst);
         if (Seconds < 0)
             lst.Add(nameof(Seconds) + " must be >= 0");
+
+        if (AsOfUTC == default)
+            lst.Add(nameof(AsOfUTC) + " must be set");
 
         if (lst.Count > 0)
             throw new ModelValidationException { Errors = lst };
