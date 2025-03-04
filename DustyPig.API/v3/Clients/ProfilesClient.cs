94212ff83a16236a130b2430a3abc1f51034b4b0
Jason Davis <jasondavis303@gmail.com>
2025-03-03 18:20:07 -0700

    Added method to list the main profile details

--- DustyPig.API/v3/Clients/ProfilesClient.cs
+++ DustyPig.API/v3/Clients/ProfilesClient.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -104,54 +105,10 @@ public class ProfilesClient
 
 
     /// <summary>
-    /// Requires profile. The avatar data must be less than 5MB. This will return the profile's new AvatarUrl
+    /// Requires profile. The <see cref="UpdateProfileAvatar.Base64Image"/> must be less than 5MB. This will return the profile's new AvatarUrl
     /// </summary>
-    public Task<Response<string>> SetProfileAvatarAsync(int id, byte[] avatar, CancellationToken cancellationToken = default)
-    {
-        const int MAX_LENGTH = 1024 * 1024 * 5;
-
-        if (avatar != null && avatar.Length == 0)
-            avatar = null;
-        ArgumentNullException.ThrowIfNull(avatar, nameof(avatar));
-
-        if (avatar.Length > MAX_LENGTH)
-        {
-            var ex = new ModelValidationException($"Validation failed: {nameof(avatar)} must not exceed {MAX_LENGTH} bytes");
-
-            if (_client.AutoThrowIfError)
-                throw ex;
-
-            return Task.FromResult(new Response<string>
-            {
-                Error = ex,
-                StatusCode = System.Net.HttpStatusCode.BadRequest,
-                ReasonPhrase = ex.Message
-            });
-        }
-
-        if (!(IsJpeg(avatar) || IsPng(avatar)))
-        {
-            var ex = new ModelValidationException($"Validation failed: {nameof(avatar)} must does not appear to be a jpg or png file");
-
-            if (_client.AutoThrowIfError)
-                throw ex;
-
-            return Task.FromResult(new Response<string>
-            {
-                Error = ex,
-                StatusCode = System.Net.HttpStatusCode.BadRequest,
-                ReasonPhrase = ex.Message
-            });
-        }
-
-        var data = new UpdateProfileAvatar
-        {
-            Id = id,
-            Base64Image = Convert.ToBase64String(avatar)
-        };
-
-        return _client.PostAsync<string>(true, PREFIX + "SetProfileAvatar", data, cancellationToken);
-    }
+    public Task<Response<string>> SetProfileAvatarAsync(UpdateProfileAvatar data, CancellationToken cancellationToken = default)
+        => _client.PostAsync<string>(true, PREFIX + "SetProfileAvatar", data, cancellationToken);
 
 
     /// <summary>
@@ -161,31 +118,4 @@ public class ProfilesClient
         _client.GetAsync<DetailedProfile>(true, PREFIX + "GetMainProfileDetails", cancellationToken);
 
 
-
-    static bool IsJpeg(byte[] data)
-    {
-        const int MIN_JPG_LENGTH = 107;
-
-        return
-            data.Length >= MIN_JPG_LENGTH &&
-            data[0] == 0xFF &&
-            data[1] == 0xD8;
-    }
-
-
-    static bool IsPng(byte[] data)
-    {
-        const int MIN_PNG_LENGTH = 67;
-        
-        return
-            data.Length >= MIN_PNG_LENGTH &&
-            data[0] == 0x89 &&
-            data[1] == 0x50 &&
-            data[2] == 0x4E &&
-            data[3] == 0x47 &&
-            data[4] == 0x0D &&
-            data[5] == 0x0A &&
-            data[6] == 0x1A &&
-            data[7] == 0x0A;
-    }
 }
--- DustyPig.API/v3/Models/UpdateProfileAvatar.cs
+++ DustyPig.API/v3/Models/UpdateProfileAvatar.cs
@@ -1,5 +1,8 @@
 ﻿using DustyPig.API.v3.Interfaces;
+using DustyPig.REST;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DustyPig.API.v3.Models;
 
@@ -7,6 +10,9 @@ public class UpdateProfileAvatar : IValidate
 {
     public int Id { get; set; }
 
+    /// <summary>
+    /// This must be less than 5MB
+    /// </summary>
     public string Base64Image { get; set; }
 
     public void Validate()
@@ -15,10 +21,48 @@ public class UpdateProfileAvatar : IValidate
 
         Validators.ValidateId(nameof(Id), Id, lst);
 
-        if(string.IsNullOrWhiteSpace(Base64Image))
+        const int MAX_LENGTH = 1024 * 1024 * 5;
+
+        if (string.IsNullOrWhiteSpace(Base64Image))
             lst.Add($"{nameof(Base64Image)} cannot be null");
+
+        if (Base64Image.Length > MAX_LENGTH)
+            lst.Add($"Validation failed: {nameof(Base64Image)} must not exceed {MAX_LENGTH} bytes");
+
+        if (!IsPngOrJpg(Convert.FromBase64String(Base64Image)))
+            lst.Add($"{nameof(Base64Image)} does not appear to be either png or jpg");
 
         if (lst.Count > 0)
             throw new ModelValidationException { Errors = lst };
     }
+
+    static bool IsPngOrJpg(byte[] data) => IsPng(data) || IsJpeg(data);
+
+    static bool IsJpeg(byte[] data)
+    {
+        const int MIN_JPG_LENGTH = 107;
+
+        return
+            data.Length >= MIN_JPG_LENGTH &&
+            data[0] == 0xFF &&
+            data[1] == 0xD8;
+    }
+
+
+    static bool IsPng(byte[] data)
+    {
+        const int MIN_PNG_LENGTH = 67;
+
+        return
+            data.Length >= MIN_PNG_LENGTH &&
+            data[0] == 0x89 &&
+            data[1] == 0x50 &&
+            data[2] == 0x4E &&
+            data[3] == 0x47 &&
+            data[4] == 0x0D &&
+            data[5] == 0x0A &&
+            data[6] == 0x1A &&
+            data[7] == 0x0A;
+    }
+
 }
